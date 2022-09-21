69906d3b0f2f21c12d2db9e30c01ad3af5287132
keeganlynch67 <70238779+keeganlynch67@users.noreply.github.com>
2022-09-21 14:09:17 -0600

    Add files via upload

---
+++ CursedWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursedWeatherAppApp.swift
+//  CursedWeatherApp
+//
+//  Created by Keegan Lynch on 9/18/22.
+//
+
+import SwiftUI
+
+@main
+struct CursedWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
