2069a35390454c42f55f995ac07c1fcf99d4be81
keeganlynch67 <70238779+keeganlynch67@users.noreply.github.com>
2022-11-28 13:57:13 -0700

    newest version

---
+++ Views/WeatherView.swift
@@ -0,0 +1,102 @@
+//
+//  WeatherView.swift
+//  FinalWeatherApp
+//
+//  Created by Keegan Lynch on 11/28/22.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var weather: ResponseBody
+    
+    var body: some View {
+        ZStack(alignment: .leading){
+            VStack{
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(weather.name)
+                        .bold().font(.title)
+                    
+                    Text("Today, \(Date().formatted(.dateTime .month().day().hour().minute()))")
+                        .fontWeight(.light)
+                }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Spacer()
+                VStack{
+                    HStack{
+                        VStack(spacing: 20) {
+                            Image(systemName: "cloud.sun.fill")
+                                .font(.system(size: 40))
+                            Text(weather.weather[0].main)
+                        }
+                        .frame(width: 150, alignment: .leading)
+                        
+                        Spacer()
+                        
+                        Text(weather.main.feels_like .roundDouble() + "°")
+                            .font(.system(size:90))
+                            .fontWeight(.bold)
+                            .padding()
+                    }
+                    
+                    Spacer()
+                        .frame(height: 80)
+                    
+                    AsyncImage(url: URL(string: "https://cdn.pixabay.com/photo/2020/01/24/21/33/city-4791269_960_720.png")) { image in
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 350)
+                        
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    
+                    Spacer()
+                    
+                }
+                .frame(maxWidth: .infinity)
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+        
+            VStack{
+                Spacer()
+                
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Weather now")
+                        .bold().padding(.bottom)
+                    
+                    HStack {
+                        WeatherRow(logo: "thermometer.low", name: "Min temp", value:(weather.main.temp_min.roundDouble()+"°"))
+                        Spacer()
+                        WeatherRow(logo: "thermometer.high", name: "Max temp", value:(weather.main.temp.roundDouble()+"°"))
+                    }
+                    HStack {
+                        WeatherRow(logo: "wind", name: "Wind speed", value:(weather.wind.speed.roundDouble()+"°"))
+                        Spacer()
+                        WeatherRow(logo: "humidity", name: "Max temp", value:(weather.main.humidity.roundDouble()+"%"))
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .padding(.bottom, 20)
+                .foregroundColor(Color(hue: 0.772, saturation: 1.0, brightness: 0.36))
+                .background(.white)
+                .cornerRadius(20, corners: [.topLeft, .topRight])
+            }
+            
+        }
+        .edgesIgnoringSafeArea(.bottom)
+        .background(Color(hue: 0.772, saturation: 1.0, brightness: 0.36))
+        .preferredColorScheme(.dark)
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView(weather: previewWeather)
+    }
+}
+
