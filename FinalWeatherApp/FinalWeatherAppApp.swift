b1033ba7d26d338ef5f027fbcbd220e5e73877a1
keeganlynch67 <70238779+keeganlynch67@users.noreply.github.com>
2022-11-28 13:20:23 -0700

    Initial Commit

---
+++ FinalWeatherApp/FinalWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalWeatherAppApp.swift
+//  FinalWeatherApp
+//
+//  Created by Keegan Lynch on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct FinalWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
